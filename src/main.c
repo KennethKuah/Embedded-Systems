dfdf31aee513b2c1b5352d0cbd4da81f93d85ff0
BaconPotatoCat <kobinfong11@gmail.com>
2023-11-26 14:59:53 +0800

    fix: missing dependancies

--- src/main.c
+++ src/main.c
@@ -1,39 +1,28 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
 #include "deauth/deauth.h"
-#include "picowi_defs.h"
-#include "picowi_pico.h"
-#include "picowi_wifi.h"
-#include "picowi_ioctl.h"
-#include "picowi_event.h"
-#include "picowi_join.h"
-#include "picowi_ip.h"
-#include "picowi_dhcp.h"
-#include "picowi_net.h"
-#include "picowi_tcp.h"
-#include "picowi_web.h"
-#include "picowi_init.h"
-#define SSID "cat"
-#define PASSWD "catcatcat"
+#include "dhcp_flood/dhcp_flood.h"
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+#define SSID "SSID"
+#define PASSWD "PASSWORD"
 
 int main()
-{
-    io_init();
+{    
+    stdio_init_all();
     sleep_ms(5000);
-    set_display_mode(DISP_INFO | DISP_JOIN | DISP_TCP_STATE | DISP_DATA);
-    if (net_init() && net_join(SSID, PASSWD))
+    if (cyw43_arch_init())
     {
-        while (1)
-        {
-            sleep_ms(1000);
+        printf("failed to initialise\n");
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+    for (;;) {
+        if (joinAP(SSID, PASSWD)) {
             deauth();
-            net_event_poll();
-            net_state_poll();
+            //send_dhcp_discover();
         }
     }
-    // for (;;) {
-    //     sleep_ms(3000);
-    //     deauth();
-    // }
-    return 0;
 }
+
