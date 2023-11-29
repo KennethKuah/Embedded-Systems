1c7b3a52afa1a6667a1d785534097b7665fde244
BaconPotatoCat <kobinfong11@gmail.com>
2023-11-29 12:19:30 +0800

    update: deauth header

--- src/deauth/deauth.h
+++ src/deauth/deauth.h
@@ -2,12 +2,14 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "lwip/pbuf.h"
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+    void set_payload(struct pbuf *p, const char *data, u16_t data_len);
     bool joinAP(char *ssid, char *password);
     void deauth();
 
